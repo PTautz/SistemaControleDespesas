6503729f8d930608bfdad8a36388d8ba13930463
Patricia <pantocbt@gmail.com>
2021-08-29 19:02:49 -0300

    Adiciona estrutura inicial do projeto e classe que mapeia a despesa

---
+++ src/main/java/domain/Despesa.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Despesa {
+    private BigDecimal valor;
+    private String nomeDespesa;
+    private LocalDateTime dataDespesa;
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getNomeDespesa() {
+        return nomeDespesa;
+    }
+
+    public void setNomeDespesa(String nomeDespesa) {
+        this.nomeDespesa = nomeDespesa;
+    }
+
+    public LocalDateTime getDataDespesa() {
+        return dataDespesa;
+    }
+
+    public void setDataDespesa(LocalDateTime dataDespesa) {
+        this.dataDespesa = dataDespesa;
+    }
+}
